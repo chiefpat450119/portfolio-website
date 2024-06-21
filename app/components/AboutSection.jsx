6642f70f79094ac53a9fd883761c372a3572d049
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-06-20 23:30:34 -0700

    About section completed

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+      </ul>
+    )
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Frontend Developer at Company A</li>
+        <li>Backend Developer at Company B</li>
+        <li>Full Stack Developer at Company C</li>
+      </ul>
+    )
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript Certification</li>
+        <li>React Certification</li>
+        <li>Node.js Certification</li>
+        <li>MongoDB Certification</li>
+      </ul>
+    )
+  }
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack developer with a passion for building web
+            applications. I have experience working with JavaScript, React,
+            Node.js, and MongoDB. I am always looking to learn new technologies
+            and improve my skills. I am currently looking for new opportunities
+            to work on exciting projects.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              Experience{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">{TAB_DATA.find((data) => data.id === tab).content}</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
