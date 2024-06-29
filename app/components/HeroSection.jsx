4866451e2b07f2314894a23b6703ea61659e7838
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-06-28 21:11:42 -0700

    changed styling and polished stuff

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 
@@ -6,18 +6,18 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-12">
-      <div className="col-span-7 place-self-center sm:text-left">
-        <section>
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-8 place-self-center sm:text-left justify-self-start">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hi, I'm{" "}
             </span>
             <TypeAnimation
               sequence={[
                 "Patrick",
                 1000,
-                "a programmer", 
+                "a programmer",
                 1000,
                 "a scripter",
                 1000,
@@ -35,30 +35,30 @@ const HeroSection = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire Me
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:from-primary-700 hover:to-secondary-700 text-white">
+              Contact Me
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:from-primary-700 hover:to-secondary-700 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 {" "}
                 Resume
               </span>
             </button>
           </div>
-        </section>
-      </div>
-      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-          <Image
-            src="/images/pfp.jpg"
-            alt="hero image"
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            width={300}
-            height={300}
-          ></Image>
+        </div>
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/pfp.jpg"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            ></Image>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
