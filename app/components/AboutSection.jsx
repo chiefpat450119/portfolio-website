fdefb39d0a65c7b31e7aec55f326c300717f141b
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-07-15 21:21:38 -0700

    Updated images and icon

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -71,12 +71,22 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image
-          src="/images/about-image.png"
-          width={500}
-          height={500}
-          alt="about image"
-        />
+        <div className="flex flex-col">
+          <Image
+            src="/images/about-image.webp"
+            width={500}
+            height={500}
+            alt="about image"
+            className="py-4"
+          />
+          <Image
+            src="/images/about-image-2.webp"
+            width={500}
+            height={500}
+            alt="about image"
+            className="py-4"
+          />
+        </div>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-400">
             About Patrick Zhou
